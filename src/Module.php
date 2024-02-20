93159200052ec78698c39c7466d1076f399a07e2
provdigi <purnachandra.valluri@gmail.com>
2024-02-20 17:45:26 +0530

    sec

---
+++ src/Module.php
@@ -0,0 +1,35 @@
+<?php
+
+namespace siripravi\nyiixta;
+
+use Yii;
+
+/**
+ * admin module definition class
+ */
+class Module extends \yii\base\Module
+{
+    /**
+     * @inheritdoc
+     */
+    public $controllerNamespace = 'siripravi\nyiixta\controllers';
+
+    /**
+     * @inheritdoc
+     */
+    public function init()
+    {
+       // $this->layout = '@app/views/layouts/base';	
+        parent::init();
+        $this->setComponents([            
+			'db' => [
+				'class' => 'yii\db\Connection',			
+                'dsn' => 'sqlite:@nyiixta/data/invoicedb.db',
+                'username' => 'root',
+                'password' => '',
+                'charset' => 'utf8',				
+			   ]
+			]);
+    }
+
+}
