0bce4493309d5c9c55376dc4a9ee775561ce455a
provdigi <purnachandra.valluri@gmail.com>
2024-02-20 23:24:17 +0530

    db

--- src/Module.php
+++ src/Module.php
@@ -28,7 +28,7 @@ class Module extends \yii\base\Module
             $this->setComponents([
                 'db' =>[
                     'class' => 'yii\db\Connection',
-                    'dsn' => 'sqlite:@nyiixta/data/invoicedb.db',
+                    'dsn' => 'sqlite:@siripravi/nyiixta/data/invoicedb.db',
                     'username' => 'root',
                     'password' => '',
                     'charset' => 'utf8',
